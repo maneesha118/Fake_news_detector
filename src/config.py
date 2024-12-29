ef606819943a074dc5c2909faeb27d0d425f1c1e
Maneesha Komati <maneeshakomati@Maneeshas-Air.lan>
2024-12-29 18:11:02 -0500

    Initial commit

---
+++ src/config.py
@@ -0,0 +1,10 @@
+class Config:
+    HUGGINGFACE_API_KEY = "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"
+    NEWS_API_KEY = "0fbf70f03808412bbe3a0c9a5646d229"
+    MODEL_ID = "facebook/bart-large-mnli"
+    TRUSTED_SOURCES = [
+        "reuters.com",
+        "apnews.com",
+        "bbc.com",
+        "bloomberg.com"
+    ]
